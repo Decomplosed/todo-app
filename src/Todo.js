3862ffc6d988b159f2e6b4d0ae3aef2654a6fec0
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-02 14:51:01 +0100

    added edit button and implemented aria label for both of them

--- src/Todo.js
+++ src/Todo.js
@@ -4,6 +4,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import CheckBox from '@material-ui/core/Checkbox'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 
 function Todo({ task, completed }) {
@@ -12,8 +13,11 @@ function Todo({ task, completed }) {
       <CheckBox tabIndex={-1} checked={completed} />
       <ListItemText>{task}</ListItemText>
       <ListItemSecondaryAction>
-        <IconButton>
+        <IconButton aria-label='Delete'>
           <DeleteIcon />
+        </IconButton>
+        <IconButton aria-label='Edit'>
+          <EditIcon />
         </IconButton>
       </ListItemSecondaryAction>
     </ListItem>
