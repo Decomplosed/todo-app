c6f69bb1c227563b5d1b8565a4ad41259378a2c8
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-02 14:46:24 +0100

    added more components from material ui

--- src/Todo.js
+++ src/Todo.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import CheckBox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/icons/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 
 function Todo({ task, completed }) {
   return (
