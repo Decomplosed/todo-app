2491109f5d7e900fb62473d014eb0e53f7445eee
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-02 14:45:04 +0100

    added CheckBox to evety Todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import CheckBox from '@material-ui/core/Checkbox'
 
 function Todo({ task, completed }) {
   return (
     <ListItem>
+      <CheckBox tabIndex={-1} checked={completed} />
       <ListItemText>{task}</ListItemText>
     </ListItem>
   )
