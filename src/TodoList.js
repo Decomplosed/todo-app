5b2357cbc84849fc8d6042d508c8ac9d304fb584
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-02 14:04:32 +0100

    imported material ui components

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider'
 
 function TodoList(props) {
   return (
-    <div>
+    <Paper>
       {props.todos.map(todo => (
         <li>{todo.task}</li>
       ))}
-    </div>
+    </Paper>
   )
 }
 
