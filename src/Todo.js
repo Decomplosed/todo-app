7b6b10d49f079b8b8ef00f09a34f3284682e66cd
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-02 15:28:56 +0100

    added functionality to EditForm

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import EditTodoForm from './EditTodoForm'
 import useToggleState from './hooks/useToggleState'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
@@ -8,27 +9,34 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 
-function Todo({ id, task, completed, removeTodo, toggleTodo }) {
+function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
+  const [isEditing, toggle] = useToggleState(false)
   return (
     <ListItem>
-      <CheckBox
-        tabIndex={-1}
-        checked={completed}
-        onClick={() => toggleTodo(id)}
-      />
-      <ListItemText
-        style={{ textDecoration: completed ? 'line-through' : 'none' }}
-      >
-        {task}
-      </ListItemText>
-      <ListItemSecondaryAction>
-        <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
-          <DeleteIcon />
-        </IconButton>
-        <IconButton aria-label='Edit'>
-          <EditIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
+      {isEditing ? (
+        <EditTodoForm id={id} editTodo={editTodo} task={task} toggleEditForm={toggle}/>
+      ) : (
+        <>
+          <CheckBox
+            tabIndex={-1}
+            checked={completed}
+            onClick={() => toggleTodo(id)}
+          />
+          <ListItemText
+            style={{ textDecoration: completed ? 'line-through' : 'none' }}
+          >
+            {task}
+          </ListItemText>
+          <ListItemSecondaryAction>
+            <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label='Edit' onClick={toggle}>
+              <EditIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </>
+      )}
     </ListItem>
   )
 }
