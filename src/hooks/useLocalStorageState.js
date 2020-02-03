ca411137fc6b1321f0fb9eeabe4806063b1b4169
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-03 15:41:35 +0100

    return state and setState

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -15,6 +15,8 @@ function UseLocalStorageState(key, defaultVal) {
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state))
   }, [key, state])
+
+  return [state, setState]
 }
 
 export default UseLocalStorageState
