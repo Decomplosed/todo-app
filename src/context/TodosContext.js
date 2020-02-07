3f936514b3e56d6d6649c95e0c6c72618cc104a6
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-07 16:35:16 +0100

    wrapped props.children in Dispatch Privider

--- src/context/TodosContext.js
+++ src/context/TodosContext.js
@@ -14,7 +14,7 @@ export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodos)
   return (
     <TodosContext.Provider value={{ todos, dispatch }}>
-      {props.children}
+      <DispatchContext.Provider>{props.children}</DispatchContext.Provider>
     </TodosContext.Provider>
   )
 }
