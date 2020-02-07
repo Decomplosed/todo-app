404244c1c16e42ceee7ca6ef7b96ed5f09d97a12
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-07 11:57:49 +0100

    import useTodoState hook, export TodosContext and added defaultTodos

--- src/context/TodosContext.js
+++ src/context/TodosContext.js
@@ -1,3 +1,9 @@
-import React, { CreateContext } from 'react'
+import React, { createContext } from 'react'
+import useTodoState from '../hooks/useTodoState'
 
-const TodosContext = createContext()
+const defaultTodos = [
+  { id: 1, task: 'Mow the lawn', completed: false },
+  { id: 2, task: 'Release the kraken', completed: true }
+]
+
+export const TodosContext = createContext()
