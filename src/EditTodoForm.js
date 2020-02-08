3d9780aebb4514c89479324496f8462b2cc8269c
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-08 20:42:17 +0100

    changed TodosContext to be DispathContext

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
 import TextField from '@material-ui/core/TextField'
 import useInputState from './hooks/useInputState'
-import { TodosContext } from './context/TodosContext'
+import { DispatchContext } from './context/TodosContext'
 
 function EditTodoForm({ id, task, toggleEditForm }) {
-  const { dispatch } = useContext(TodosContext)
+  const { dispatch } = useContext(DispatchContext)
   const [value, handleChange, reset] = useInputState(task)
 
   return (
