dae34c32a72c0ceeaa4d305335dc9def93899b82
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-08 21:54:28 +0100

    import useLocalStorageReducer

--- src/context/TodosContext.js
+++ src/context/TodosContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from 'react'
+import { useLocalStorageReducer } from '../hooks/useLocalStorageReducer'
 import todoReducer from '../reducers/TodoReducer'
 
 const defaultTodos = [
