9bd8ead7fa5dc837892a2c6f136681b09ee0df5f
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-08 21:47:25 +0100

    useReducer

--- src/hooks/useLocalStorageReducer.js
+++ src/hooks/useLocalStorageReducer.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useReducer,  useEffect } from 'react'
 
 function UseLocalStorageState(key, defaultVal) {
   const [state, setState] = useState(() => {
